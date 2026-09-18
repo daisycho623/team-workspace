d99519c6b480749951dc8f65874379dc0157cd73
조윤영 <daisycho06@lgupluspartners.co.kr>
2026-09-18 17:15:22 +0900

    260918 업무 관리 파일 업로드

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# team-workspace
+# CX 업무 관리 대시보드
+
+Google Sheets `CX` 탭을 읽는 반응형 HTML/CSS/JS 대시보드입니다. `index.html`을 정적 서버에서 열면 공개 시트 데이터를 불러오며, 공개 데이터가 비어 있으면 샘플 데이터로 UI를 표시합니다.
---
+++ config.js
@@ -0,0 +1,2 @@
+// Apps Script를 웹 앱으로 배포한 후 생성된 /exec URL을 입력하세요.
+window.APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwJPRTdE3Sj_kjlrJ57skdfnQu-P6Lp8sVzR5TYaMRkyM093kZWMEM6SYt9-F5owzDkzQ/exec';
