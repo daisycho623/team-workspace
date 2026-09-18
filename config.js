43a83c6c6974b41983c5c8924d9f4e82d4e7c86c
조윤영 <daisycho06@lgupluspartners.co.kr>
2026-09-18 18:17:35 +0900

    260918 수정

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Apps Script를 웹 앱으로 배포한 후 생성된 /exec URL을 입력하세요.
-window.APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzd7FVaOE7s6DKiTeX6p_wj-bozgIk_XZIN0D-_WSf9zLR2hUQyv2F1jzSosAh_y7eiKw/exec';
+window.APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxFDtFmozhnBmycDmkehHfXpA0J2wiraLIN1AbDw9KjbsVGNt3cTrtIn2q7tvOktcFwvQ/exec';
